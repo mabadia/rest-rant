332f7588c594d78956104dc6c13023441e2609d6
mabadia <angel.beyou95@gmail.com>
2023-07-24 18:43:32 -0700

    ;

--- controllers/places.js
+++ controllers/places.js
@@ -1,9 +1,9 @@
-const router = require('express').Router()
+const router = require('express').Router();
 // GET /places
 router.get('/', (req, res) => {
-    let places = []
-    res.render('places/index', { places })
-})
+    let places = [];
+    res.render('places/index', { places });
+});
 // More code here in a moment
 
-module.exports = router
+module.exports = router;
--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const app = express();
 // Express Settings
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
 // Controller & Routes
-app.use('/places', require('./controllers/places'))
+app.use('/places', require('./controllers/places'));
 
 app.get('/', (req, res) => {
-    res.render('home') 
-})
+    const places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }];
+    res.render('home');
+});
 
 app.get('*', (req, res) => {
     res.render('error404')
-})
+});
 //listen for Connections
-app.listen(process.env.PORT)
+app.listen(process.env.PORT);
 
