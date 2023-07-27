c252d9b68ef04f90959eef70a12d63c421c7bc4a
mabadia <angel.beyou95@gmail.com>
2023-07-26 19:45:32 -0700

    navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -9,6 +9,18 @@ function Def(html) {
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous" />
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">HOME</a></li>
+                        <li>
+                            <a href="/places">Places</a>
+                        </li>
+                        <li>
+                            <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>
