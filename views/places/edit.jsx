800a199001c8a25525e72cdb4a2ce64f780fff55
mabadia <angel.beyou95@gmail.com>
2023-08-07 21:25:37 -0700

    day 11 part 3

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -30,6 +30,14 @@ function edit_form(data) {
                             <label htmlFor="founded">Founding Year</label>
                             <input className="form-control" id="founded" name="founded" value={data.place.founded} />
                         </div>
+                        <div className="form-group col-sm-4">
+                            <label htmlFor="founded">Founded</label>
+                            <input className="form-control"
+                                id="founded"
+                                name="founded"
+                                value={data.place.founded}
+                            />
+                        </div>
                     </div>
                     <div className="form-group">
                         <label htmlFor="cuisines">Cuisines</label>
@@ -44,3 +52,14 @@ function edit_form(data) {
 }
 
 module.exports = edit_form
+
+    < h1 > Edit Place</h1 >
+        <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+            ...
+            <div className="row">
+                ...
+
+            </div>
+            ...
+            <input className="btn btn-primary" type="submit" value="Update Place" />
+        </form>
